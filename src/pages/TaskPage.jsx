cb1afb37ac426378ce844a5f3e40f30487518a25
Rafael Kotovicz <rafaelrck21@gmail.com>
2025-02-09 20:18:25 -0300

    correção da tela de tarefas

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -9,7 +9,7 @@ export default function TaskPage(){
     const description = searchParams.get("description")
     return (
         <div className="w-screen h-screen bg-slate-500 p-6">
-            <div className="w-[500] space-y-4">
+            <div className="w-[500] mx-auto space-y-4">
                 <div className="flex justify-center relative mb-6">
                     <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
                         <ChevronLeftIcon/>
